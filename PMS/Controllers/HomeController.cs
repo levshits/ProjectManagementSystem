fa93f4d9d052033ed697dc77bbf82d768f9690a2
levshits <levshits.l@gmail.com>
2015-12-12 16:25:50 +0300

    Configuration fixed

---
+++ PMS/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using Levshits.Web.Common.Controllers;
+
+namespace PMS.Web.Controllers
+{
+    public class HomeController: BaseController
+    {
+        public override ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
