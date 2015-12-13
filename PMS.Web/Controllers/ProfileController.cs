9e2beb74e442828e6d196e095610edcf8521d4fb
levshits <levshits.l@gmail.com>
2015-12-13 17:06:10 +0300

    add profile controller

---
+++ PMS.Web/Controllers/ProfileController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+using PMS.Web.Models;
+
+namespace PMS.Web.Controllers
+{
+    public class ProfileController: PrivateController
+    {
+        public const string IndexAction = "Index";
+        public const string Name = "Profile";
+
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var model = new UserDetailsModel();
+            return View(model);
+        }
+    }
+}
